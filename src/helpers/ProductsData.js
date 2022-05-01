e5841c5fb28fc178b26f7ceab466646266e75a51
Aj Candelaria <ajajcandelaria@gmail.com>
2022-05-01 22:43:30 +0400

    Update ProductsData.js

--- src/helpers/ProductsData.js
+++ src/helpers/ProductsData.js
@@ -3,6 +3,32 @@ import product2 from "../images/duo1.png";
 import product3 from "../images/shampoo1.png";
 import product4 from "../images/duo1.png";
 
+export const getShampoo = [
+  {
+    itemId: 0,
+    itemImage: product1,
+    itemName: "Shea Moisture Coconut & Hibiscus Curl & Shine Shampoo",
+    itemPrice: 649.00,
+    itemDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper auctor neque vitae tempus quam pellentesque nec nam.",
+    itemHowToUse: "Egestas sed sed risus pretium quam vulputate dignissim. Aliquam ultrices sagittis orci a scelerisque purus semper. ",
+    itemIngredients: "Felis donec, et odio, pellentesque, diam volutpat, commodo, sed egestas. Etiam erat velit scelerisque in dictum non consectetur a. Sit amet luctus venenatis lectus.",
+    itemStocks: 1,
+  }
+]
+
+
+export const getConditioner = [
+  {
+    itemId: 0,
+    itemImage: product1,
+    itemName: "Shea Moisture Coconut & Hibiscus Curl & Shine Shampoo",
+    itemPrice: 649.00,
+    itemDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper auctor neque vitae tempus quam pellentesque nec nam.",
+    itemHowToUse: "Egestas sed sed risus pretium quam vulputate dignissim. Aliquam ultrices sagittis orci a scelerisque purus semper. ",
+    itemIngredients: "Felis donec, et odio, pellentesque, diam volutpat, commodo, sed egestas. Etiam erat velit scelerisque in dictum non consectetur a. Sit amet luctus venenatis lectus.",
+    itemStocks: 1,
+  }
+]
 
 export const getProduct = [
   {
@@ -10,7 +36,7 @@ export const getProduct = [
     itemImage: product1,
     itemCategory: "Shampoo",
     itemLink: "/products/shampoo",
-    itemDescription: "1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    itemDescription: "1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     itemId: 1,
@@ -34,3 +60,4 @@ export const getProduct = [
     itemDescription: "4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   },
 ]
+
